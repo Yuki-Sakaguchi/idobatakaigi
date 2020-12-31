879db9c0b97984de8ea7faa4aba1c4662fa69ff0
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2020-12-31 16:48:04 +0900

    update signin page

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SignIn from './SignIn';
 
 export default () => {
+  const [name, setName] = useState('');
+  console.log({ name });
+
   return (
-    <SignIn />
+    <SignIn setName={setName} />
   );
 };
