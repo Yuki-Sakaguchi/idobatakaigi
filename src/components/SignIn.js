106d78e788fb1e24f3bff3477afa884105da40cb
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2020-12-31 17:20:56 +0900

    update save name by enter key

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -66,6 +66,12 @@ export default function SignIn({ setName }) {
             name="name"
             autoFocus
             onChange={(e) => setString(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault();
+                setName(string);
+              }
+            }}
           />
           <Button
             type="button"
